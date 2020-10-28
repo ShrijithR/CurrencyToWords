ca00502ec920c9570b6f100fa485981a5f8fcbba
ShrijithR <72496419+ShrijithR@users.noreply.github.com>
2020-10-28 15:38:44 +0530

    Update CurrencyToWords.py

--- CurrencyToWords.py
+++ CurrencyToWords.py
@@ -1,9 +1,9 @@
 from src.Support_Functions import convert
 import time
-"""Loops until the appropriate input is received
-Calls the convert function from the convert module"""
 
 def main():
+    """Loops until the appropriate input is received
+    Calls the convert function from the convert module"""
     while True:
         try:
             Number = input('Please enter the amount you want to convert or ctrl+c to exit\n')
